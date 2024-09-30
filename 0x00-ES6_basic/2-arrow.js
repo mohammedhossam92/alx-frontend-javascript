ffcad795065d363683b3f00f577edcbdbf7b5eca
mohammed hossam <mohammedhossam5000@gmail.com>
2024-09-30 17:41:38 +0000

    modify to follow lint

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -5,4 +5,5 @@ export default function getNeighborhoodsList() {
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
-  };}
+  };
+}
