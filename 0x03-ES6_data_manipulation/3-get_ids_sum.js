03ca32a47783d22dcea0205d908553c03058b624
Mohammed Hossam <113324663+mohammedhossam92@users.noreply.github.com>
2024-10-08 06:05:52 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,7 @@ export default function getStudentIdsSum(students) {
   if (Object.getPrototypeOf(students) === Array.prototype) {
     const ids = students.map((items) => items.id);
     const reducer = (accumlator, currentValue) => accumlator + currentValue;
-      return ids.reduce(reducer);
+    return ids.reduce(reducer);
     }
     return [];
-  }
+}
