33586154a09034916740c57d5639a153f535e85f
mohammed hossam <mohammedhossam5000@gmail.com>
2025-01-25 11:27:35 +0000

    js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  // check arg is an array before using filter
   if (Object.getPrototypeOf(students) === Array.prototype) {
     return students.filter((items) => items.location === city);
   }
